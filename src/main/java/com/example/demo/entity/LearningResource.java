8adfc714f2a42e23ff18f26f4444bcb825cfa1a1
Bindushree <bindushree.14ask@gmail.com>
2022-08-03 14:49:26 +0530

    3-Methods-Collections

--- src/main/java/com/example/demo/entity/LearningResource.java
+++ src/main/java/com/example/demo/entity/LearningResource.java
@@ -5,64 +5,67 @@ import java.time.LocalDate;
 public class LearningResource {
 	private int id;
 	private String name;
-	private int costPrice;
-	private int sellingPrice;
-	private LearningResourceStatus productStatus;
+	private Double costPrice;
+	private Double sellingPrice;
+	private LearningResourceStatus learningResourceStatus;
 	private LocalDate createdDate;
 	private LocalDate publishedDate;
 	private LocalDate retiredDate;
 	
-	public LearningResource(int id, String name, int costPrice, int sellingPrice, LearningResourceStatus productStatus,
+	public LearningResource() {
+    }
+	
+	public LearningResource(int id, String name, Double costPrice, Double sellingPrice, LearningResourceStatus productStatus,
 			LocalDate createdDate, LocalDate publishedDate, LocalDate retiredDate) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.costPrice = costPrice;
 		this.sellingPrice = sellingPrice;
-		this.productStatus = productStatus;
+		this.learningResourceStatus = productStatus;
 		this.createdDate = createdDate;
 		this.publishedDate = publishedDate;
 		this.retiredDate = retiredDate;
 	}
 
-	public int getId() {
+	public int getLearningResourceId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setLearningResourceId(int id) {
 		this.id = id;
 	}
 
-	public String getName() {
+	public String getLearningResourceName() {
 		return name;
 	}
 
-	public void setName(String name) {
+	public void setLearningResourceName(String name) {
 		this.name = name;
 	}
 
-	public int getCostPrice() {
+	public Double getCostPrice() {
 		return costPrice;
 	}
 
-	public void setCostPrice(int costPrice) {
-		this.costPrice = costPrice;
+	public void setCostPrice(Double costPrice2) {
+		this.costPrice = costPrice2;
 	}
 
-	public int getSellingPrice() {
+	public Double getSellingPrice() {
 		return sellingPrice;
 	}
 
-	public void setSellingPrice(int sellingPrice) {
-		this.sellingPrice = sellingPrice;
+	public void setSellingPrice(Double sellingPrice2) {
+		this.sellingPrice = sellingPrice2;
 	}
 
-	public LearningResourceStatus getProductStatus() {
-		return productStatus;
+	public LearningResourceStatus getLearningResourceStatus() {
+		return learningResourceStatus;
 	}
 
-	public void setProductStatus(LearningResourceStatus productStatus) {
-		this.productStatus = productStatus;
+	public void setLearningResourceStatus(LearningResourceStatus productStatus) {
+		this.learningResourceStatus = productStatus;
 	}
 
 	public LocalDate getCreatedDate() {
@@ -88,5 +91,6 @@ public class LearningResource {
 	public void setRetiredDate(LocalDate retiredDate) {
 		this.retiredDate = retiredDate;
 	}
+
 	
 }
